23c8505a65229ae6d60dc81b05eb257c98ecbda0
Darius Bakunas-Milanowski <dbakuna@us.ibm.com>
2021-05-27 10:40:49 -0400

    fix: add tenantID to theme colors resource id

--- appid/data_source_appid_theme_color.go
+++ appid/data_source_appid_theme_color.go
@@ -2,6 +2,7 @@ package appid
 
 import (
 	"context"
+	"fmt"
 
 	appid "github.com/IBM/appid-go-sdk/appidmanagementv4"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
@@ -45,7 +46,7 @@ func dataSourceAppIDThemeColorRead(ctx context.Context, d *schema.ResourceData, 
 		d.Set("header_color", *colors.HeaderColor)
 	}
 
-	d.SetId("themeColors")
+	d.SetId(fmt.Sprintf("%s/themeColors", tenantID))
 
 	return diags
 }
